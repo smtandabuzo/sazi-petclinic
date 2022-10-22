c8a8cc6058bec8a1df1f75cc60c6fb90300120ad
Sazi Mtandabuzo <s_mtandabuzo@yahoo.com>
2022-10-22 13:16:30 +0200

    Initial commit

---
+++ src/main/java/sazi/springframework/sazipetclinic/SaziPetclinicApplication.java
@@ -0,0 +1,13 @@
+package sazi.springframework.sazipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaziPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaziPetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sazi/springframework/sazipetclinic/SaziPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sazi.springframework.sazipetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaziPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
