a1089ed93d984124d2306dfc5f8fc748eb0eb068
Sazi Mtandabuzo <s_mtandabuzo@yahoo.com>
2022-11-11 15:54:03 +0200

    Refactored Map Impl to abstract ID generation. Closes #23.

---
+++ pet-clinic-web/src/main/java/sazi/springframework/petclinic/bootstrap/DataLoader.java
@@ -0,0 +1,50 @@
+package sazi.springframework.petclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import sazi.springframework.petclinic.model.Owner;
+import sazi.springframework.petclinic.model.Vet;
+import sazi.springframework.petclinic.services.OwnerService;
+import sazi.springframework.petclinic.services.VetService;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded Owners....");
+
+        Vet vet1 = new Vet();
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+        vetService.save(vet2);
+
+        System.out.println("Loaded Vets....");
+    }
+}
