4f99ac8b6a5a627eda4db129b3b7ec90d7a97597
Sazi Mtandabuzo <s_mtandabuzo@yahoo.com>
2022-11-11 14:59:36 +0200

    Added Owners List and updated controller. Closes #16.

--- pet-clinic-web/src/main/java/sazi/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/sazi/springframework/petclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package sazi.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sazi.springframework.petclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
