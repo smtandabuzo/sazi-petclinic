fda2a6de51cb229ee101f450a49ec3054e54cf9f
Sazi Mtandabuzo <s_mtandabuzo@yahoo.com>
2022-11-26 14:49:50 +0200

    Updated Owner With JPA Entity Mappings. Closes #36.

--- pet-clinic-data/src/main/java/sazi/springframework/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/sazi/springframework/petclinic/model/Person.java
@@ -1,9 +1,14 @@
 package sazi.springframework.petclinic.model;
 
-public class Person extends BaseEntity {
-    private String firstName;
-    private String lastName;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 
+@MappedSuperclass
+public class Person extends BaseEntity {
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
     public String getFirstName() {
         return firstName;
     }
