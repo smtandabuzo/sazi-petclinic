e59d2bac8c301e5cad765210a29fda1a03bce8bc
Sazi Mtandabuzo <s_mtandabuzo@yahoo.com>
2023-02-01 07:42:19 +0200

    Display owner record.Closes #53

--- pet-clinic-web/src/main/java/sazi/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/sazi/springframework/petclinic/controllers/OwnerController.java
@@ -2,7 +2,10 @@ package sazi.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import sazi.springframework.petclinic.services.OwnerService;
 
 @RequestMapping("/owners")
@@ -23,5 +26,12 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners(){
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
---
+++ pet-clinic-web/src/test/java/sazi/springframework/petclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,60 @@
+package sazi.springframework.petclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import sazi.springframework.petclinic.model.Owner;
+import sazi.springframework.petclinic.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController ownerController;
+
+    Set<Owner> ownerSet;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        ownerSet = new HashSet<>();
+        ownerSet.add(Owner.builder().id(1L).build());
+        ownerSet.add(Owner.builder().id(2L).build());
+        mockMvc = MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @Test
+    void listOwners() {
+        //when(ownerService.findAll()).thenReturn(ownerSet);
+        //mockMvc.perform(get("/"));
+    }
+
+    @Test
+    void findOwners() {
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
+
+       // mockMvc.perform(get("/owners/123"))
+        //        .andExpect(status().isOk())
+        //        .andExpect(view().name("owners/ownerDetails"))
+         //       .andExpect(model().attribute("owner", hasProperty("id", is(1l))));
+    }
+}
